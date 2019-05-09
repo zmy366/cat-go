1d2c9d0fb60dae19a9fe2f15fe4b13ad37796277
zhangmy <zhangmy366@gmail.com>
2019-05-09 17:03:34 +0800

    fix bug: get http port from xml

--- cat/config.go
+++ cat/config.go
@@ -32,6 +32,7 @@ type XMLConfigServers struct {
 type XMLConfigServer struct {
 	Host string `xml:"ip,attr"`
 	Port int    `xml:"port,attr"`
+	HttpPort int `xml:"http-port,attr"`
 }
 
 var config = Config{
@@ -81,7 +82,10 @@ func parseXMLConfig(data []byte) (err error) {
 		logger.Warning("Failed to parse xml content")
 	}
 
-	for _, x := range c.Servers.Servers {
+	for index, x := range c.Servers.Servers {
+		if index == 0 {
+			config.httpServerPort = x.HttpPort
+		}
 		config.httpServerAddresses = append(config.httpServerAddresses, serverAddress{
 			host: x.Host,
 			port: x.Port,
